4f527891617d39a4123783e01906415c63c82317
Kennlf <kenn741q@stud.kea.dk>
2021-09-20 02:05:16 +0200

    caesarEncrypt metode complete / decrypt+encrypt menu skrevet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,6 @@
 package com.company;
 
-import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -9,7 +9,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         //Velkomst -
         //Du skal træffe et valg - 0 exit, 1 Numbercipher, 2 caesar, 3 Veginère
-        while (true) {
+       // while (true) {
             System.out.println("Velkomme til Enigma maskinen");
             System.out.println("Du skal nu først vælge hvilken metode du ønsker at anvende");
             System.out.println("Du har følgende muligheder:");
@@ -22,8 +22,24 @@ public class Main {
 
             if (firstChoice == 1) {
 
-
             } else if (firstChoice == 2) {
+                System.out.println("Nu skal du vælge om du vil encrypte eller decrypte");
+                System.out.println("E) Encrypt");
+                System.out.println("D) Decrypt");
+                System.out.println("Indtast nu E/D:");
+                int secondChoice = sc.nextInt();
+                encryptCaesarMenu();
+                /*if ((secondChoice).equals('e')) {
+                    encryptCaesarMenu();
+                    /*String plaintText = sc.nextLine();
+                    System.out.println("Du skal nu indtaste den tekst du ønsker at kry");
+                    int shift = sc.nextInt();
+                    String encryptetText = caesarEncrypt(plaintText, shift);
+                    System.out.println(encryptetText);
+                } if else (secondChoice == 2) {
+                    decryptCaesarMenu();
+
+                } */
 
             } else if (firstChoice == 3) {
 
@@ -39,9 +55,19 @@ public class Main {
             //2 Caesar - Encrypt og decrypt metode
             //3 Veginènre - Encrypt og decrypt metode
         }
-    }
+
 
     public static void decryptCaesarMenu() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Velkommen til decryption af caesar code");
+        System.out.println("Du skal nu indtaste den ciphertext ønsker at decrypte:");
+        String cipherText = sc.nextLine().toUpperCase();
+        System.out.println("Du skal nu den shiftkey som tallene er ændret med:");
+        int shift = sc.nextInt();
+        String plaintText = caesarDecrypt(cipherText, shift);
+        System.out.println("Den decryptet tekst er som følger");
+        System.out.println(plaintText);
+
         // beder brugeren om ciphertext
         // beder brugeren om shift-værdi
         // kalder caesarDecrypt med ciphertext og shift-værdi
@@ -49,10 +75,16 @@ public class Main {
     }
 
     public static void encryptCaesarMenu() {
-        // beder brugeren om plaintext
-        // beder brugeren om shift-værdi
-        // kalder caesarEncrypt med ciphertext og shift-værdi
-        // udskriver ciphertext modtaget fra ovenstående
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Velkommen til encryption af din text med caesar code");
+        System.out.println("Du skal nu indtaste den tekst du ønsker at encrypte:");
+        String plaintText = sc.nextLine().toUpperCase();
+        System.out.println("Du skal nu den shiftkey som tallene er ændret med(Heltal):");
+        int shift = sc.nextInt();
+        String cipherText = caesarEncrypt(plaintText, shift);
+        System.out.println("Den encryptet tekst: ");
+        System.out.println(cipherText);
+        System.out.println("Og den anvendte shiftkey er: " + shift);
     }
 
     public static String listOfNumbersToText ( int[] numbers){
@@ -73,8 +105,13 @@ public class Main {
     }
 
     public static int[] textToListOfNumbers(String plainText) {
-        //Skal kalde letterToNumber
-        return new int[0];
+        int[] numbers = new int[plainText.length()];
+        char[] letters = plainText.toCharArray();
+
+        for (int i = 0; i < letters.length; i++) {
+            numbers[i] = letterToNumber(letters[i]);
+        }
+        return numbers;
     }
 
     public static int letterToNumber(char letter) {
@@ -84,10 +121,14 @@ public class Main {
     }
 
     public static String caesarEncrypt( String plaintext, int shift ) {
+        int[] numbers = textToListOfNumbers(plaintext);
+        int[] shiftednumbers = shiftListOfNumbers(numbers, shift);
+        String cipherText = listOfNumbersToText(shiftednumbers);
+
         // textToListOfNumbers
         // shiftListOfNumbers
         // listOfNumbersToText
-        return new String("");
+        return cipherText;
     }
 
     public static String caesarDecrypt( String ciphertext, int shift ) {
@@ -114,6 +155,8 @@ public class Main {
             shiftnumber = shiftnumber - maxNumberOfLetters;
         } else if (number == 0){
             shiftnumber = 0;
+        } else if ( shiftnumber <= -1) {
+            shiftnumber = shiftnumber + maxNumberOfLetters;
         }
         return shiftnumber;
     }
